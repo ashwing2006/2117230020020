49a15018d571ba2008cfd05c01b3f0053927dea6
ashwing2006 <ashwingovindaraj.2023.cse@ritchennai.edu.in>
2026-05-06 11:30:21 +0530

    Initial backend ccompleted

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const cors = require("cors")
+const logger = require("./logger")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(logger)
+
+const notifications = [
+  {
+    id: 1,
+    title: "Placement Drive",
+    type: "Placement"
+  },
+  {
+    id: 2,
+    title: "Semester Results",
+    type: "Result"
+  }
+]
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend working"
+  })
+})
+
+app.get("/notifications", (req, res) => {
+  res.status(200).json(notifications)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`)
+})
